263471c8b4348432c351c11471f225b1a78f13e7
Ko Ko Maung <122875462+JeffTF@users.noreply.github.com>
2024-06-27 23:27:18 +0630

    some changes

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hsu_bakery/hsu_cake_web.dart';
+import 'package:hsu_bakery/services/locator.dart';
+
+void main() {
+  locator();
+  runApp(const HsuCakeWeb());
+}
+
+
